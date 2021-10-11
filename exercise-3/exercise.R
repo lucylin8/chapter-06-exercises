7f85198f5f3b5e3c63a76be75938f68df42a21b8
lucylin8 <lucylin8@uw.edu>
2021-10-11 15:03:05 -0700

    finished exercises 3-4

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -7,7 +7,12 @@
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
 # function
 
-
+add_three <- function(number)
+{
+  val <- number + 3
+  return (val)
+}
+ten <- add_three(7)
 # Define a function `imperial_to_metric` that takes in two arguments: a number 
 # of feet and a number of inches
 # The function should return the equivalent length in meters
@@ -16,3 +21,11 @@
 # Create a variable `height_in_meters` by passing your height in imperial to the
 # `imperial_to_metric` function
 
+imperial_to_metric <- function(feet, inches)
+{
+   feet_and_inches <- feet * 12 + inches
+   conversion <- 0.0254
+   meters <- feet_and_inches * conversion
+   return (meters)
+}
+print((imperial_to_metric(5,0)))
--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -4,7 +4,18 @@
 # returns whether or not (e.g., a boolean) the length of one argument is greater
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
-
+is_twice_as_long <- function(char1, char2)
+{
+  if (nchar(char1)>=nchar(char2)*2) 
+  {
+    return(paste(char1, "is great than or equal to twice as long the string length of", char2))
+  }else if(nchar(char2)>=nchar(char1)*2)
+  {
+    return(paste(char2, "is great than or equal to twice as long the string length of", char1))
+  }else{
+    return(paste(char1, "and", char2, "are not twice as longer than each other")) 
+  }
+}
 
 # Call your `is_twice_as_long` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
@@ -21,4 +32,21 @@
 
 # Call your `describe_difference` function by passing it different length strings
 # to confirm that it works. Make sure to check all 3 conditions1
+
+describe_difference<- function(string1, string2)
+{
+  if (nchar(string1)>nchar(string2)) 
+  {
+    difference<-nchar(string1)-nchar(string2)
+    return (paste("you first string is longer by", difference, "characters"))
+  }else if (nchar(string2)>nchar(string1))
+  {
+    difference<-nchar(string2)-nchar(string1)
+    return (paste("you second string is longer by", difference, "characters"))
+  }else
+  {
+    return("your strings are the same length")
+  }
+  
+}
 
